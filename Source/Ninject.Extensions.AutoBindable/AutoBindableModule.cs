ab799e3b8b787773ec64f4623b615baca94c54c1
Robert Mulder <mulder@creativeid.nl>
2017-05-16 16:20:59 +0200

    Fixed an issue where assemblies that couldn't be loaded could cause the binding resolution to fail.

--- Source/Ninject.Extensions.AutoBindable/AutoBindableModule.cs
+++ Source/Ninject.Extensions.AutoBindable/AutoBindableModule.cs
@@ -1,42 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Ninject.Modules;
 
 namespace Ninject.Extensions.AutoBindable {
     public class AutoBindableModule : NinjectModule {
         public override void Load() {
-            // Get the IAutoBindable Type.
-            var autoBindInterface = typeof(IAutoBindable);
-            // Get all the types loaded in the current AppDomain
-            var allTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(i => i.GetTypes()).ToArray();
-            // Find all interfaces that extend IAutoBindable
-            var autoBindInterfaces = allTypes.Where(i => i != autoBindInterface && i.IsInterface && autoBindInterface.IsAssignableFrom(i));
-            // Find the implementations of these inheriting interfaces.
-            foreach (var autoBind in autoBindInterfaces) {
-                if (autoBind.IsGenericType) {
-                    var implementations = from t in allTypes
-                                          from i in t.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == autoBind)
-                                          where t.IsClass
-                                                && !t.IsAbstract
-                                          select new {
-                                              Type = t,
-                                              Interface = i
-                                          };
+			// Get the IAutoBindable Type.
+			var autoBindInterface = typeof(IAutoBindable);
+			// Get all the assemblies currently loaded.
+			var allTypes = GetAllTypes();
 
-                    foreach (var implementation in implementations) {
-                        //var intF = implementation.GetInterfaces().First(j => j.IsGenericType && j.GetGenericTypeDefinition() == autoBind);
-                        //var genType = intF.GenericTypeArguments[0];
-                        Bind(autoBind.MakeGenericType(implementation.Interface.GenericTypeArguments)).To(implementation.Type);
-                    }
+			BindAllTypes(autoBindInterface, allTypes);
+		}
 
-                } else {
-                    var implementations = allTypes.Where(i => i.IsClass && !i.IsAbstract && autoBind.IsAssignableFrom(i));
-                    foreach (var implementation in implementations) {
-                        // Bind the implementations automatically
-                        Bind(autoBind).To(implementation);
-                    }
-                }
-            }
-        }
-    }
+		private void BindAllTypes(Type autoBindInterface, List<Type> allTypes) {
+			// Find all interfaces that extend IAutoBindable
+			var autoBindInterfaces = allTypes.Where(i => i != autoBindInterface && i.IsInterface && autoBindInterface.IsAssignableFrom(i));
+			// Find the implementations of these inheriting interfaces.
+			foreach (var autoBind in autoBindInterfaces) {
+				if (autoBind.IsGenericType) {
+					var implementations = from t in allTypes
+										  from i in t.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == autoBind)
+										  where t.IsClass
+												&& !t.IsAbstract
+										  select new {
+											  Type = t,
+											  Interface = i
+										  };
+
+					foreach (var implementation in implementations) {
+						//var intF = implementation.GetInterfaces().First(j => j.IsGenericType && j.GetGenericTypeDefinition() == autoBind);
+						//var genType = intF.GenericTypeArguments[0];
+						Bind(autoBind.MakeGenericType(implementation.Interface.GenericTypeArguments)).To(implementation.Type);
+					}
+
+				} else {
+					var implementations = allTypes.Where(i => i.IsClass && !i.IsAbstract && autoBind.IsAssignableFrom(i));
+					foreach (var implementation in implementations) {
+						// Bind the implementations automatically
+						Bind(autoBind).To(implementation);
+					}
+				}
+			}
+		}
+
+		private static List<Type> GetAllTypes() {
+			var allAssemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+			var allTypes = new List<Type>();
+
+			foreach (var assembly in allAssemblies) {
+				try {
+					// Get all the types in the assembly
+					var typesInAssembly = assembly.GetTypes().ToArray();
+					allTypes.AddRange(typesInAssembly);
+				} catch (Exception ex) {
+					// Mey, skip this assembly
+					Trace.WriteLine(ex.ToString());
+				}
+			}
+
+			return allTypes;
+		}
+	}
 }
